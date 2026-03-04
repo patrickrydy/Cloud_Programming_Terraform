2e2c713377d1688d55052a9f3debd3babbfeab01
patrickrydy <141062688+patrickrydy@users.noreply.github.com>
2026-03-04 15:35:26 +0100

    Refine comments and variable description in secrets.tf
    Updated the description of the eodhd_api_key variable for clarity and modified comments for better understanding.

--- secrets.tf
+++ secrets.tf
@@ -1,21 +1,19 @@
 # Bereitstellung des Secrets im GCP Secret Manager
 resource "google_secret_manager_secret" "eodhd_api_key" {
   secret_id = "eodhd-api-key"
-
   replication {
     auto {}
   }
 }
 
-# Initialisierung der Secret-Version mit der lokalen Umgebungsvariable
+# Den Wert dynamisch und unsichtbar aus der Shell laden
 resource "google_secret_manager_secret_version" "eodhd_api_key_version" {
   secret      = google_secret_manager_secret.eodhd_api_key.id
   secret_data = var.eodhd_api_key
 }
 
 variable "eodhd_api_key" {
-  description = "Der geheime API Key für EODHD (wird lokal injiziert)"
+  description = "Der geheime API Key für EODHD (wird lokal über die Shell injiziert)"
   type        = string
-  sensitive   = true  #maskiert den Wert in den Terraform-Logs
-  default     = "DUMMY_KEY_FUER_DAS_DEPLOYMENT"
+  sensitive   = true  # Maskiert den Wert in den Terraform-Logs
 }
